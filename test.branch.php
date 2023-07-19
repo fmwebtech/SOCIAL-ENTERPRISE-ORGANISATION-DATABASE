9007a3467ffad252e8b595be2b505a27bdf290a8
Franklin Mwale <130289096+202004198@users.noreply.github.com>
2023-07-19 11:14:58 +0200

    updated

---
+++ test.branch.php
@@ -0,0 +1,42 @@
+<?php
+require_once('classes\class.branch.php');
+
+$mybranch = new BRANCH();
+
+
+
+//save($seoCountryId, $name, $address)
+//branchExists($name, $address)
+//edit($id, $seoCountryId, $name, $address, $status)
+//safeToEdit($id,$name
+//getBranch($seoCountryId)
+//delete($id)
+
+$mybranch ->save(1, 'Frank', 'CBU');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
