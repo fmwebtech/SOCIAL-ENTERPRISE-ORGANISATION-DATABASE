52ff266a24003c8a69727d6faa1ee41aae908125
Preston Kazhinga <75981308+pkazhinga@users.noreply.github.com>
2023-07-19 10:13:24 +0200

    Create connectionsetting.php

---
+++ settings/connectionsetting.php
@@ -0,0 +1,10 @@
+<?php
+$iudicbiwugfijhdgfiwe = '10.10.0.215';
+$tjhscjhds = 'intern';
+$iwuebfjhf = 'internpassword';
+$ifuweifwid = 'DHIPS';
+$Myconnection = new PDO("sqlsrv:host=$iudicbiwugfijhdgfiwe;dbname=$ifuweifwid",$tjhscjhds, $iwuebfjhf);
+$Myconnection -> setAttribute(PDO::ATTR_ERRMODE, 
+PDO::ERRMODE_EXCEPTION);
+//echo (explode('/',$_SERVER['PHP_SELF'])[sizeof(explode('/',$_SERVER['PHP_SELF']))-1]);
+?>
