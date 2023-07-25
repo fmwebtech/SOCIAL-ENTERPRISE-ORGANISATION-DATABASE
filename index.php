96445c740c5458b58c0f1119bea0462fcdb52ef4
Preston Kazhinga <75981308+pkazhinga@users.noreply.github.com>
2023-07-25 14:25:48 +0200

    ui

--- index.php
+++ index.php
@@ -37,170 +37,38 @@
   
 </head>
 
-<body class="bg-theme bg-theme1">
+<body class="bg-theme bg-theme11">
 
   <!-- Start wrapper-->
   <div id="wrapper">
 
-    <!--Start sidebar-wrapper-->
-    <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
-     <div class="brand-logo">
-      <a href="index.php">
-       <!-- <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon"> -->
-       <h5 class="logo-text">SEOD</h5>
-     </a>
-   </div>
-   <ul class="sidebar-menu do-nicescrol">
-    <li class="sidebar-header">MAIN NAVIGATION</li>
-    <li>
-      <a href="#" onclick="goTo('Views/SEO_table.php' , 'mainContent')" >
-        <i class="zmdi zmdi-view-dashboard"></i> <span>Social Enterprises</span>
-      </a>
-    </li>
-
-
-<!-- 
-    <li>
-      <a href="#">
-        <i class="zmdi zmdi-format-list-bulleted"></i> <span>Services</span>
-      </a>
-    </li>
-
-    <li>
-      <a href="#">
-        <i class="zmdi zmdi-format-list-bulleted"></i> <span>Products</span>
-      </a>
-    </li>
- -->
-    <li>
-      <a href="#" onclick="goTo('Views/users.php' , 'mainContent')">
-        <i class="zmdi zmdi-male-female"></i> <span>Users</span>
-      </a>
-    </li>
-
-      <!-- 
-      <li>
-        <a href="calendar.html">
-          <i class="zmdi zmdi-calendar-check"></i> <span>Calendar</span>
-          <small class="badge float-right badge-light">New</small>
-        </a>
-      </li>
-    -->
-    <li>
-      <a href="#" onclick="goTo('Views/profile.php' , 'mainContent')">
-        <i class="zmdi zmdi-face"></i> <span>Profile</span>
-      </a>
-    </li>
-
-
-     <li>
-      <a href="#" onclick="goTo('Views/home.php' , 'mainContent')">
-        <i class="zmdi zmdi-face"></i> <span>Reports</span>
-      </a>
-    </li>
-
-
-
-    <li>
-      <a href="login.html" target="_blank">
-        <i class="zmdi zmdi-lock"></i> <span>Login</span>
-      </a>
-    </li>
-
-    <li>
-      <a href="#" target="_blank">
-        <i class="zmdi zmdi-lock"></i> <span>Log out</span>
-      </a>
-    </li>
-
-      <!--  <li>
-        <a href="register.html" target="_blank">
-          <i class="zmdi zmdi-account-circle"></i> <span>Registration</span>
-        </a>
-      </li> -->
-
-     <!--  <li class="sidebar-header">LABELS</li>
-      <li><a href="javaScript:void();"><i class="zmdi zmdi-coffee text-danger"></i> <span>Important</span></a></li>
-      <li><a href="javaScript:void();"><i class="zmdi zmdi-chart-donut text-success"></i> <span>Warning</span></a></li>
-      <li><a href="javaScript:void();"><i class="zmdi zmdi-share text-info"></i> <span>Information</span></a></li> -->
-
-    </ul>
-
-  </div>
+  <!--Statr sidebar-wrapper-->
+  <?php
+	include('sideBar.php');
+  ?>
   <!--End sidebar-wrapper-->
+  
+  
+  
+  
+  
 
   <!--Start topbar header-->
-  <header class="topbar-nav">
-   <nav class="navbar navbar-expand fixed-top">
-    <ul class="navbar-nav mr-auto align-items-center">
-      <li class="nav-item">
-        <a class="nav-link toggle-menu" href="javascript:void();">
-         <i class="icon-menu menu-icon"></i>
-       </a>
-     </li>
-     <li class="nav-item">
-      <form class="search-bar">
-        <input type="text" class="form-control" placeholder="Enter keywords">
-        <a href="javascript:void();"><i class="icon-magnifier"></i></a>
-      </form>
-    </li>
-  </ul>
+	<?php include('topBar.php')?>
+  <!--End topbar header-->
 
-  <ul class="navbar-nav align-items-center right-nav-link">
-    <li class="nav-item dropdown-lg">
-      <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();">
-        <i class="fa fa-envelope-open-o"></i></a>
-      </li>
-      <li class="nav-item dropdown-lg">
-        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();">
-          <i class="fa fa-bell-o"></i></a>
-        </li>
-        <li class="nav-item language">
-          <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();"><i class="fa fa-flag"></i></a>
-          <ul class="dropdown-menu dropdown-menu-right">
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-gb mr-2"></i> English</li>
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-fr mr-2"></i> French</li>
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-cn mr-2"></i> Chinese</li>
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-de mr-2"></i> German</li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
-            <span class="user-profile"><img src="https://via.placeholder.com/110x110" class="img-circle" alt="user avatar"></span>
-          </a>
-          <ul class="dropdown-menu dropdown-menu-right">
-           <li class="dropdown-item user-details">
-            <a href="javaScript:void();">
-             <div class="media">
-               <div class="avatar"><img class="align-self-start mr-3" src="https://via.placeholder.com/110x110" alt="user avatar"></div>
-               <div class="media-body">
-                <h6 class="mt-2 user-title">Sarajhon Mccoy</h6>
-                <p class="user-subtitle">mccoy@example.com</p>
-              </div>
-            </div>
-          </a>
-        </li>
-        <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-envelope mr-2"></i> Inbox</li>
-        <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-wallet mr-2"></i> Account</li>
-        <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-settings mr-2"></i> Setting</li>
-        <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-power mr-2"></i> Logout</li>
-      </ul>
-    </li>
-  </ul>
-</nav>
-</header>
-<!--End topbar header-->
+
+
+
+
+
 
 <div class="clearfix"></div>
 
 <div class="content-wrapper">
   <div class="container-fluid" id="mainContent">
 
-    <?php require  'Views/home.php'; ?>
+    <?php //require  'Views/home.php'; ?>
 
 
     <!--start overlay-->
@@ -216,13 +84,7 @@
 <!--End Back To Top Button-->
 
 <!--Start footer-->
-<footer class="footer">
-  <div class="container">
-    <div class="text-center">
-      Copyright © 2022 Copperbelt University
-    </div>
-  </div>
-</footer>
+	<?php include('footer.php')?>
 <!--End footer-->
 
 <!--start color switcher-->
---
+++ sideBar.php
@@ -0,0 +1,85 @@
+<!--Start sidebar-wrapper-->
+    <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
+     <div class="brand-logo">
+      <a href="index.php">
+       <!-- <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon"> -->
+       <h5 class="logo-text">SEOD</h5>
+     </a>
+   </div>
+   <ul class="sidebar-menu do-nicescrol">
+    <li class="sidebar-header">MAIN NAVIGATION</li>
+    <li>
+      <a href="#" onclick="goTo('Views/SEO_table.php' , 'mainContent')" >
+        <i class="zmdi zmdi-view-dashboard"></i> <span>Social Enterprises</span>
+      </a>
+    </li>
+
+
+<!-- 
+    <li>
+      <a href="#">
+        <i class="zmdi zmdi-format-list-bulleted"></i> <span>Services</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <i class="zmdi zmdi-format-list-bulleted"></i> <span>Products</span>
+      </a>
+    </li>
+ -->
+    <li>
+      <a href="#" onclick="goTo('Views/users.php' , 'mainContent')">
+        <i class="zmdi zmdi-male-female"></i> <span>Users</span>
+      </a>
+    </li>
+
+      <!-- 
+      <li>
+        <a href="calendar.html">
+          <i class="zmdi zmdi-calendar-check"></i> <span>Calendar</span>
+          <small class="badge float-right badge-light">New</small>
+        </a>
+      </li>
+    -->
+    <li>
+      <a href="#" onclick="goTo('Views/profile.php' , 'mainContent')">
+        <i class="zmdi zmdi-face"></i> <span>Profile</span>
+      </a>
+    </li>
+
+
+     <li>
+      <a href="#" onclick="goTo('Views/home.php' , 'mainContent')">
+        <i class="zmdi zmdi-face"></i> <span>Reports</span>
+      </a>
+    </li>
+
+
+
+    <li>
+      <a href="login.html" target="_blank">
+        <i class="zmdi zmdi-lock"></i> <span>Login</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#" target="_blank">
+        <i class="zmdi zmdi-lock"></i> <span>Log out</span>
+      </a>
+    </li>
+
+      <!--  <li>
+        <a href="register.html" target="_blank">
+          <i class="zmdi zmdi-account-circle"></i> <span>Registration</span>
+        </a>
+      </li> -->
+
+     <!--  <li class="sidebar-header">LABELS</li>
+      <li><a href="javaScript:void();"><i class="zmdi zmdi-coffee text-danger"></i> <span>Important</span></a></li>
+      <li><a href="javaScript:void();"><i class="zmdi zmdi-chart-donut text-success"></i> <span>Warning</span></a></li>
+      <li><a href="javaScript:void();"><i class="zmdi zmdi-share text-info"></i> <span>Information</span></a></li> -->
+
+    </ul>
+
+  </div>
