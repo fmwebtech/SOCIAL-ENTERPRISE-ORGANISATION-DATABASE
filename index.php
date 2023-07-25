ed483cd1406b8cf068b5a979c0b32bc3037791b1
Preston Kazhinga <75981308+pkazhinga@users.noreply.github.com>
2023-07-25 14:40:05 +0200

    login topbar
    login topbar

--- index.php
+++ index.php
@@ -47,30 +47,34 @@
 	include('sideBar.php');
   ?>
   <!--End sidebar-wrapper-->
-  
-  
-  
-  
-  
 
   <!--Start topbar header-->
 	<?php include('topBar.php')?>
   <!--End topbar header-->
-
-
-
-
-
-
 
 <div class="clearfix"></div>
 
 <div class="content-wrapper">
   <div class="container-fluid" id="mainContent">
 
-    <?php //require  'Views/home.php'; ?>
+    <div class="card mt-3">
+    <div class="card-content">
+	
+	
+	
+	
+	
+	
+	
+	 <!--content comes here-->
 
 
+
+
+
+
+	</div>
+	</div>
     <!--start overlay-->
     <div class="overlay toggle-menu"></div>
     <!--end overlay-->
@@ -86,44 +90,6 @@
 <!--Start footer-->
 	<?php include('footer.php')?>
 <!--End footer-->
-
-<!--start color switcher-->
-<div class="right-sidebar">
-  <div class="switcher-icon">
-    <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
-  </div>
-  <div class="right-sidebar-content">
-
-    <p class="mb-0">Gaussion Texture</p>
-    <hr>
-
-    <ul class="switcher">
-      <li id="theme1"></li>
-      <li id="theme2"></li>
-      <li id="theme3"></li>
-      <li id="theme4"></li>
-      <li id="theme5"></li>
-      <li id="theme6"></li>
-    </ul>
-
-    <p class="mb-0">Gradient Background</p>
-    <hr>
-
-    <ul class="switcher">
-      <li id="theme7"></li>
-      <li id="theme8"></li>
-      <li id="theme9"></li>
-      <li id="theme10"></li>
-      <li id="theme11"></li>
-      <li id="theme12"></li>
-      <li id="theme13"></li>
-      <li id="theme14"></li>
-      <li id="theme15"></li>
-    </ul>
-
-  </div>
-</div>
-<!--end color switcher-->
 
 </div><!--End wrapper-->
 
--- topBar.php
+++ topBar.php
@@ -1,3 +1,10 @@
+  <?php
+  @session_start();
+  require_once('classes/user.php');
+  $myUserObject = new USER($_SESSION['id']);
+  
+  ?>
+  
   <header class="topbar-nav">
    <nav class="navbar navbar-expand fixed-top">
     <ul class="navbar-nav mr-auto align-items-center">
@@ -22,16 +29,8 @@
       <li class="nav-item dropdown-lg">
         <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();">
           <i class="fa fa-bell-o"></i></a>
-        </li>
-        <li class="nav-item language">
-          <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();"><i class="fa fa-flag"></i></a>
-          <ul class="dropdown-menu dropdown-menu-right">
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-gb mr-2"></i> English</li>
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-fr mr-2"></i> French</li>
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-cn mr-2"></i> Chinese</li>
-            <li class="dropdown-item"> <i class="flag-icon flag-icon-de mr-2"></i> German</li>
-          </ul>
-        </li>
+       </li>
+       
         <li class="nav-item">
           <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
             <span class="user-profile"><img src="https://via.placeholder.com/110x110" class="img-circle" alt="user avatar"></span>
@@ -42,20 +41,15 @@
              <div class="media">
                <div class="avatar"><img class="align-self-start mr-3" src="https://via.placeholder.com/110x110" alt="user avatar"></div>
                <div class="media-body">
-                <h6 class="mt-2 user-title">Sarajhon Mccoy</h6>
-                <p class="user-subtitle">mccoy@example.com</p>
+                <h6 class="mt-2 user-title"><?php echo $myUserObject->firstName.' '.$myUserObject->lastName; ?></h6>
+                <p class="user-subtitle"><?php echo $myUserObject->email; ?></p>
               </div>
             </div>
           </a>
         </li>
+     
         <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-envelope mr-2"></i> Inbox</li>
-        <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-wallet mr-2"></i> Account</li>
-        <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-settings mr-2"></i> Setting</li>
-        <li class="dropdown-divider"></li>
-        <li class="dropdown-item"><i class="icon-power mr-2"></i> Logout</li>
+        <li class="dropdown-item"><i class="icon-power mr-2"></i> <a href="logout.php"> Logout</a></li>
       </ul>
     </li>
   </ul>
