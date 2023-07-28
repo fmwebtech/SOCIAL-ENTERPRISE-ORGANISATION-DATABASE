5928a2948785842e17656fdf5db4f61b1d2b5119
Franklin Mwale <130289096+202004198@users.noreply.github.com>
2023-07-28 11:43:59 +0200

    Update seos.php

--- seos.php
+++ seos.php
@@ -112,32 +112,36 @@
 
       <h4 class="modal-title text-dark">Add SEO</h4><button type="button" class="close" data-dismiss="modal">&times;</button>
     </div>
-    <div class="modal-body">
+     <form id="addSeoForm"> 
+      <div class="modal-body">
 
-    <form id="addSeoForm">
-      <div class="form-group">
-        <b class="col-6">SEO name</b>
-        <input type="" name = "name" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter Established year">
-        
-      
-      </div>
+  
+      <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+              <b class="col-6">SEO name</b>
+              <input type="" name = "name" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter Established year">
+              
+            
+            </div>
 
-      <div class="form-group">
-        <b class="col-6">Established</b>
-        <input type="number" name = "established" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter Established year">
-      </div>
+            <div class="form-group">
+              <b class="col-6">Established</b>
+              <input type="number" name = "established" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter Established year">
+            </div>
 
-      <div class="form-group">
-        <b class="col-6">Ownership</b>
-        <input type="number" name = "ownership" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter Ownership">
-      </div>
+            <div class="form-group">
+              <b class="col-6">Ownership</b>
+              <input type="number" name = "ownership" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter Ownership">
+            </div>
 
-      <div class="form-group">
-        <b class="col-6">Governance</b>
-        <input type="number" name = "governance" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter governance">
-      </div>
+            <div class="form-group">
+              <b class="col-6">Governance</b>
+              <input type="number" name = "governance" class="form-control form-control-rounded" value="" id="input-6" placeholder="Enter governance">
+            </div>
 
-      <div class="form-group">
+
+            <div class="form-group">
         <b class="col-6">Primary Country</b>
         <select name ="primaryCountry" class="form-control form-control-rounded" >
           <?php
@@ -149,6 +153,13 @@
           ?>
         </select>
       </div>
+
+        </div>
+
+
+        <div class="col-lg-6">
+
+     
 
 
       <div class="form-group">
@@ -193,9 +204,10 @@
 
 
       
+      </div>
 
 
-
+    </div>
     </div>
     <div class="modal-footer">
 
