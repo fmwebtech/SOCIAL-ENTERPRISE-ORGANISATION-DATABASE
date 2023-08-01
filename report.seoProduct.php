93980e786a785136a854332d1ffdf8304b9a27f3
HinkxB <132204160+HinkxB@users.noreply.github.com>
2023-08-01 13:56:41 +0200

    Create report.seoProduct.php

---
+++ report.seoProduct.php
@@ -0,0 +1,174 @@
+
+<?php
+	@session_start();
+	if(!isset($_SESSION['email']))
+	{
+		header('location:login.php');
+	}
+
+	require_once('classes/class.authorization.php');
+	if(!(new AUTHORIZATION($_SESSION['id'],(explode('/',$_SERVER['PHP_SELF'])[sizeof(explode('/',$_SERVER['PHP_SELF']))-1])))->authorize())
+	{
+		header('location:logout.php');
+	}
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+  <title>SOCIAL ENTERPRISE ORGANISATION DATABASE</title>
+  <!-- loader-->
+  <link href="assets/css/pace.min.css" rel="stylesheet"/>
+  <script src="assets/js/pace.min.js"></script>
+  <!--favicon-->
+  <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+  <!-- Vector CSS -->
+  <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+  <!-- simplebar CSS-->
+  <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet"/>
+  <!-- Bootstrap core CSS-->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+  <!-- animate CSS-->
+  <link href="assets/css/animate.css" rel="stylesheet" type="text/css"/>
+  <!-- Icons CSS-->
+  <link href="assets/css/icons.css" rel="stylesheet" type="text/css"/>
+  <!-- Sidebar CSS-->
+  <link href="assets/css/sidebar-menu.css" rel="stylesheet"/>
+  <!-- Custom Style-->
+  <link href="assets/css/app-style.css" rel="stylesheet"/>
+  
+</head>
+
+<body class="bg-theme bg-theme11">
+
+  <!-- Start wrapper-->
+  <div id="wrapper">
+
+  <!--Statr sidebar-wrapper-->
+  <?php
+	include('sideBar.php');
+  ?>
+  <!--End sidebar-wrapper-->
+
+  <!--Start topbar header-->
+	<?php include('topBar.php')?>
+  <!--End topbar header-->
+
+<div class="clearfix"></div>
+
+<div class="content-wrapper">
+  <div class="container-fluid" id="mainContent">
+
+    <div class="card mt-3">
+    <div class="card-content">
+	
+	<div class="card-content">
+	
+	
+	
+	<!-- My table  -->
+    <div class="row">
+
+		<div class="col-lg-12">
+		  <div class="card">
+			<div class="card-body">
+			  <h5 class="card-title">Report Seo Products  <button onclick="openModal()"  type="button" class="btn btn-light btn-round btn-sm px-5 pull-right">Add User Profile</button></h5>
+			  <div class="table-responsive">
+				<table class="table table-hover" id="myUserTable">
+				  <thead>
+					<tr>
+					  <th scope="col">#</th>
+					  <th scope="col">SEO</th>
+					  <th scope="col">Product</th>
+					  <th scope="col">Currency</th>
+
+					</tr>
+				  </thead>
+				  <tbody id="reportPool">
+				  
+					
+				   
+				  </tbody>
+				</table>
+			  </div>
+			</div>
+		  </div>
+		</div>
+		</div>
+		</div>
+	
+	
+	
+	
+	
+	 <!--content comes here           hello         -->
+
+
+
+
+
+
+	</div>
+	</div>
+    <!--start overlay-->
+    <div class="overlay toggle-menu"></div>
+    <!--end overlay-->
+
+  </div>
+  <!-- End container-fluid-->
+
+</div><!--End content-wrapper-->
+<!--Start Back To Top Button-->
+<a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
+<!--End Back To Top Button-->
+
+<!--Start footer-->
+	<?php include('footer.php')?>
+<!--End footer-->
+
+</div><!--End wrapper-->
+
+<!-- Bootstrap core JavaScript-->
+
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<!-- simplebar js -->
+<script src="assets/plugins/simplebar/js/simplebar.js"></script>
+<!-- sidebar-menu js -->
+<script src="assets/js/sidebar-menu.js"></script>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+
+<script type="text/javascript">
+
+  function goTo(url , id)
+  {
+   $('#'+id).html('<img class="col-12" src="assets/images/loading.gif" />');
+   $.ajax({ url: url,
+    data: { },
+
+    type: 'post',
+    success: function(output) {
+
+      $('#'+id).html(output);
+    }
+  });
+ }
+</script>
+
+</body>
+</html>
