e8ba0f60a0e6bba57990733c431035e62432edf7
Sydney164 <nkakasydney164@gmail.com>
2023-08-01 09:25:11 +0200

    sydney

--- classes/class.country.php
+++ classes/class.country.php
@@ -2,6 +2,11 @@
 //Frank
 require_once('settings\connectionsetting.php');
 require_once('classes\class.logs.php');
+require_once('classes/class.authorization.php');
+	if(!(new AUTHORIZATION($_SESSION['id'],(explode('/',$_SERVER['PHP_SELF'])[sizeof(explode('/',$_SERVER['PHP_SELF']))-1])))->authorize())
+	{
+		header('location:logout.php');
+	}
 class COUNTRY
 {
 
